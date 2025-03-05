17993ead40aa0d99a28c2aa22eb46e1bbf9479cd
Shaik Zuheruddin <119682715+Shaik-Zuher@users.noreply.github.com>
2025-03-05 13:19:55 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,67 @@
+//Type function
+const auto_typed=document.getElementById("auto-type")
+var typed=new Typed(auto_typed,{ 
+    strings: ["Front End Developer","Software Developer"],
+    typeSpeed:100,
+    backSpeed:25,
+    loop:true
+})
+const navToggle = document.getElementById('nav-toggle');
+const navlist = document.getElementById('navy');
+
+navToggle.addEventListener('click', () => {
+    if(navlist.style.display=="flex"){
+        navlist.style.display="none"
+    }
+    else{
+    navlist.style.display="flex"
+    }
+});
+
+/*==================== REMOVE MENU MOBILE ====================*/
+/*
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+/*
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); */
